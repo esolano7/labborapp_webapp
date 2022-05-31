6751ea11164cb58cf05aff4b34688cc7c17ed384
esolano7 <esolano7@yahoo.com>
2022-05-31 08:33:38 -0600

    localstorage

--- assets/js/onesignal.js
+++ assets/js/onesignal.js
@@ -1,6 +1,5 @@
 const query = new URLSearchParams(window.location.search)
 const onesignal_push_id = query.get('onesignal_push_id')
 if (onesignal_push_id) {
-  localStorage.clear()
   localStorage.setItem('onesignal_push_id', onesignal_push_id)
 }
